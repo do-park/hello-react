66aa637095442ca5862e339cdf1d64295dd7b984
박도희 <dohee.pa@gmail.com>
2021-02-11 18:59:35 +0900

    3 컴포넌트

---
+++ src/Say.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+const Say = () => {
+  const [message, setMessage] = useState('');
+  const onClickEnter = () => setMessage('Hello react');
+  const onClickLeave = () => setMessage('Good bye');
+
+  const [color, setColor] = useState('black');
+  return (
+    <div>
+      <button onClick={onClickEnter}>Enter</button>
+      <button onClick={onClickLeave}>Leave</button>
+      <h1 style={{ color }}>{message}</h1>
+      <button style={{ color: 'red' }} onClick={() => setColor('red')}>Red</button>
+      <button style={{ color: 'green' }} onClick={() => setColor('green')}>Green</button>
+      <button style={{ color: 'blue' }} onClick={() => setColor('blue')}>Blue</button>
+    </div>
+  );
+};
+
+export default Say;
