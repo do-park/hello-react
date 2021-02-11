590d64a1330b285649ec2e10f5125704b4a23cd4
박도희 <dohee.pa@gmail.com>
2021-02-11 18:45:54 +0900

    3.4.1 클래스형 컴포넌트의 state

--- src/Counter.js
+++ src/Counter.js
@@ -1,27 +1,28 @@
-import React, { useReducer } from 'react';
+import React, { Component } from 'react';
 
-function reducer(state, action) {
-  switch (action.type) {
-    case 'INCREMENT':
-      return { value: state.value + 1 };
-    case 'DECREMENT':
-      return { value: state.value - 1 };
-    default:
-      return state;
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 0,
+      fixedNumber: 0
+    };
   }
-}
-
-const Counter = () => {
-  const [state, dispatch] = useReducer(reducer, { value: 0 });
-  return (
-    <div>
-      <p>
-        현재 카운터 값은 <b>{state.value}입니다.</b>
-      </p>
-      <button onClick={() => dispatch({ type: 'INCREMENT' })}>+1</button>
-      <button onClick={() => dispatch({ type: 'DECREMENT' })}>-1</button>
-    </div>
-  );
+  render() {
+    const { number, fixedNumber } = this.state;
+    return (
+      <div>
+        <h1>{number}</h1>
+        <h2>바뀌지 않는 값: {fixedNumber}</h2>
+        <button onClick={() => {
+          this.setState({ number: number + 1 });
+        }}
+        >
+          + 1
+        </button>
+      </div>
+    )
+  }
 };
 
 export default Counter;
